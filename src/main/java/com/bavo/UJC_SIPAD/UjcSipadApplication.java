fda64ddf24db30d2c57b8d053c3b62200ae471b9
Abilio Bavo <abiliobavo@gmail.com>
2025-06-06 09:37:52 +0200

    criacao do projecto e configuracoes iniciais

---
+++ src/main/java/com/bavo/UJC_SIPAD/UjcSipadApplication.java
@@ -0,0 +1,13 @@
+package com.bavo.UJC_SIPAD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UjcSipadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UjcSipadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bavo/UJC_SIPAD/UjcSipadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bavo.UJC_SIPAD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UjcSipadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
